d2a3c4fea98c7e4f5cdb3096ccc54ece86314efc
annas-dev-685 <annas.developer@gmail.com>
2025-06-24 02:34:55 +0000

    add logo

--- application/logs/log-2025-06-24.php
+++ application/logs/log-2025-06-24.php
@@ -4,3 +4,9 @@ ERROR - 2025-06-24 01:50:08 --> Severity: error --> Exception: Call to undefined
 ERROR - 2025-06-24 01:51:55 --> Severity: error --> Exception: Call to undefined function mysqli_init() /var/www/html/system/database/drivers/mysqli/mysqli_driver.php 143
 ERROR - 2025-06-24 01:51:57 --> Severity: error --> Exception: Call to undefined function mysqli_init() /var/www/html/system/database/drivers/mysqli/mysqli_driver.php 143
 ERROR - 2025-06-24 01:52:31 --> Severity: error --> Exception: Call to undefined function mysqli_init() /var/www/html/system/database/drivers/mysqli/mysqli_driver.php 143
+ERROR - 2025-06-24 09:24:50 --> Your server does not support the GD function required to process this type of image.
+ERROR - 2025-06-24 09:24:50 --> PNG images are not supported.
+ERROR - 2025-06-24 09:28:20 --> Your server does not support the GD function required to process this type of image.
+ERROR - 2025-06-24 09:28:20 --> PNG images are not supported.
+ERROR - 2025-06-24 09:32:25 --> Your server does not support the GD function required to process this type of image.
+ERROR - 2025-06-24 09:32:25 --> PNG images are not supported.
